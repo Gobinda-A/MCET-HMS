8e20fb1df64c5d31c86f18759836b298d1e83590
Gobu1208 <g.a.jnv.1208@gmail.com>
2022-11-22 03:16:44 +0530

    New Page loading issue Resolved

--- log.js
+++ log.js
@@ -1,14 +1,13 @@
-let uname='Gobu1208';
-let pass='Gobu123';
-var val=true;
-function verifyMe(){
-    var un=document.forms['loginForm']["uname"].value;
-    var pw=document.forms['loginForm']["pass"].value;
-    if(un==uname && pw==pass){
-    window.location="new.html";val=true;
-
-}
-    else
-    {alert("Inavlid Username or Password");val=false;}
-return val;
+document.getElementById("myAnchor").addEventListener("click", function(event){
+    event.preventDefault()
+  });
+          function verifyMe(){
+              let uname='Gobu1208';
+              let pass='Gobu123';
+      var un=document.getElementById("uname").value;
+      var pw=document.getElementById("lock").value;
+      if(un==uname && pw==pass){
+      location.replace("new.html");
   }
+      else
+  alert("Inavlid Username or Password");}
